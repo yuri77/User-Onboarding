ed28674aac569f246e967e0af472312f395c8c49
yurika sakae <yurika.sun@gmail.com>
2019-08-07 14:57:51 -0700

    basic required form field populated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import FormikForm from "./components/UserForm";
 
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <FormikForm />
+    </div>
+  );
 }
 
 export default App;
